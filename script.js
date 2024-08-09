f4f0cd3aebb4d406fbb18e8279d8ed93005bf22a
Ayaan Adrito <ayaan.adrito@gmail.com>
2024-08-08 22:46:18 -0400

    Add base calculating functions

---
+++ script.js
@@ -0,0 +1,48 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case "+":
+            return add(num1, num2)
+        case "-":
+            return subtract(num1, num2)
+        case "*":
+            return multiply(num1, num2)
+        case "/":
+            return divide(num1, num2)
+    }
+}
+
+/*val1 = Number(prompt("enter a value: "));
+op = prompt("Enter an operator: ")
+val2 = Number(prompt("enter another value: "));
+
+alert(operate(op, val1, val2));*/
+
+const container = document.querySelector(".container");
+
+function make_button(val) {
+    const button = document.createElement("button");
+    button.textContent = val;
+    button.addEventListener("click", () => {alert(button.textContent)})
+    container.appendChild(button);
+}
+
+make_button(1);
+make_button(2);
+make_button(3);
+make_button(4);
